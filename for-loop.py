006e45e11f5336d177feff6b355e0075dfc103a2
saparboyevmagrur-blip <saparboyevmagrur.com>
2026-02-24 18:11:20 +0500

    practice: for_loop for list

--- for-loop.py
+++ for-loop.py
@@ -98,7 +98,7 @@ for index in range(len(sonlar)):
 #     telefonlar.append(telefon)
 # print(telefonlar)
 
-numbers = [69, 18, 89, 12, 75, 82, 5, 26]
+# numbers = [69, 18, 89, 12, 75, 82, 5, 26]
 # min_value = min(numbers)
 # results = []
 # for number in numbers:
@@ -136,7 +136,7 @@ numbers = [69, 18, 89, 12, 75, 82, 5, 26]
     
 # print(kopaytma / s)
 
-nums = [24, 50, 72, 96, 95]
+# nums = [24, 50, 72, 96, 95]
 # s = 0
 # for son in nums:
 #     s += son ** 2
@@ -150,17 +150,130 @@ nums = [24, 50, 72, 96, 95]
 # k = s / length
 # print(k)
 
-nums = [12, -5, 15, 89, -75, -18]
+# nums = [12, -5, 15, 89, -75, -18]
 # musbat elementlar yig'indisi
 # manfiy elementlar ko'paytmasi
-s = 0
-k = 1
-for son in nums:
-    # print(son)
-    if son > 0:
-        s += son
-    else:
-        k *= son
+# s = 0
+# k = 1
+# for son in nums:
+#     # print(son)
+#     if son > 0:
+#         s += son
+#     else:
+#         k *= son
 
-print(s)
-print(k)
+# print(s)
+# print(k)
+
+# 1 dan 20 gacha bo'lgan sonlardan juftlarini ko'paytmasini toqlarini yig'indisiga nisbati
+# kopaytma = 1
+# s = 0
+# for n in range(1, 20):
+#     if n % 2 == 0:
+#         kopaytma *= n
+#     else:
+#         s += n
+# print(kopaytma / s)
+
+# nums = [12, -5, 15, 89, -75, 18]
+# o'rta_arifmetik = s / length
+# o'rta_geometrik = ko'paytma
+# s = 0
+# for number in nums:
+#     s += number
+
+# length = len(nums)
+# average_value = s / length
+# print(average_value)
+
+# import math
+# k = 1
+# for son in nums:
+#     k *= son
+
+# ortacha = math.pow(k, 1 / len(nums))
+# print(ortacha)
+
+# numbers = [7, 97, -58, 90]
+# Plan
+# 1. Juft qiymatlar yig'indisi
+# 2. Juft qiymatlar soni
+# 3. Juftlarini o'rtachasi
+# s = 0
+# counter = 0
+# for son in numbers:
+#     if son % 2 == 0:
+#         s += son # s = s + son
+#         counter += 1
+
+# print(s / counter)
+# k = 0
+# nums = [97, 97, -92, 14, 22]
+# for number in nums:
+#     if number % 2 == 0 or number % 3 == 0 or number % 5 == 0:
+#        k += number
+# print(k) 
+
+# M = int(input("M sonni kiriting: "))
+# sonlar = [12, 88, 30, 87]
+# s = 0
+# for n in sonlar:
+#     if n > M:
+#         s += n
+# print(s)
+# import math
+# nums = [44, 34, 42, 83, 43, 64]
+# k = 1
+# for n in nums:
+#     if n % 2 == 0 or n % 5 == 0:
+#         k *= n
+# print(math.sin(k))
+
+# nums = [76, 12, 51, 50, 98, 17]
+# counter = 0
+# # plan 1. toq qiymatlar 2. toq qiymatlar yig'indisi 3. toq qiymatlar soni
+# s = 0
+# for num in nums:
+#     if num % 2 == 1:
+#         s += num
+#         counter += 1
+
+# print(s / counter)
+
+# 115
+# m = int(input("M sonni kiriting: "))
+# nums = [85, 15, 57, 68, 18, 67, 7, 45, 69, 21, 1, 5, 98, 34]
+# s = 0
+# for n in nums:
+#     if m > n:
+#         s += n ** 2
+
+# print(s)
+
+# number = [44, 59, -75, 73]
+# k = 0
+# s = 0
+# for num in number:
+#     k += num ** 2
+#     s += num
+
+# length = len(number)
+# c = s / length
+# print(k)
+# print(c)
+import math
+numbers = [7, 24, -5, 23, 99, -3, 24, 51]
+# plan 1. o'rtacha qiymati # 2. manfiy elementlari
+# 3. manfiy elementlarini o'_q log almashtirsh
+s = 0
+for number in numbers:
+    s += number
+average_value = s / len(numbers)
+log_value = math.log(average_value)
+print(log_value) # 3.31
+
+for index in range(len(numbers)):
+    if numbers[index] < 0:
+        numbers[index] = log_value
+
+print(numbers)
