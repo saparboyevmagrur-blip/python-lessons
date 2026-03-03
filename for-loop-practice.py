d1c2c613d65835dc22534b11d4d991b5090b1be6
saparboyevmagrur-blip <saparboyevmagrur.com>
2026-03-03 18:15:23 +0500

    learn: while loop

--- for-loop-practice.py
+++ for-loop-practice.py
@@ -26,11 +26,42 @@
 # 1. min element topish
 # 2. oxirgi element topish
 # 3. almashtirish
-sonlar = [74, 0, 1, 33] 
-minimum_value = min(sonlar)
-last_element = sonlar[-1]
-min_index = sonlar.index(minimum_value)
+# sonlar = [74, 0, 1, 33] 
+# minimum_value = min(sonlar)
+# last_element = sonlar[-1]
+# min_index = sonlar.index(minimum_value)
 
-sonlar[min_index] = last_element
-sonlar[-1] = minimum_value
-print(sonlar)
+# sonlar[min_index] = last_element
+# sonlar[-1] = minimum_value
+# print(sonlar)
+
+# 124
+# numbers = [29, 50, -14, 4, 27, -56]
+# k = int(input("Element o'rnini kiriting: "))
+# plan:
+# 1. max element topish
+# 2. max element indexini topish
+# 3. k o'rindagi element bilan max element o'rnini almashtirish
+# max_value = max(numbers)
+# max_index = numbers.index(max_value)
+# numbers[4] = 32
+# numbers[max_index] = numbers[k - 1]
+# numbers[k - 1] = max_value
+# print(numbers)
+
+a, b = 5, 3
+# natija: a = 3; b = 5
+# 1-usul:
+# temporary variable - vaqtinchalik o'zgaruvchi
+# c = a # 5
+# a = b # 3
+# b = c # 5
+# print(a, b)
+
+# 2-usul:
+# a, b = b, a
+# print(a, b)
+
+# 3-usul:
+[a, b] = [b, a]
+print(a, b)
---
+++ while-loop.py
@@ -0,0 +1,85 @@
+# Takrorlanish operatorlari:
+# 1. for loop
+# 2. while loop
+
+# 1 dan 10 gacha sonlar ket-ketligini chiqaring
+# for son in range(1, 11):
+#     print(son)
+
+# son = 1
+# while son < 11:
+#     print(son)
+#     son += 1
+
+# Infinity loop - cheksiz sikl
+# while True:
+#     print("Infinity loop")
+
+# while va input()
+# print("Kiritilgan sonning kvadratini qaytaruvchi dastur.")
+# savol = "Istalgan son kiriting "
+# savol += "(dasturni to'xtatish uchun 'exit' deb yozing): " # savol = savol + text
+# qiymat = ''
+# while qiymat != 'exit':
+#     qiymat = input(savol)
+#     if qiymat != 'exit':
+#         print(float(qiymat) ** 2)
+
+# break(sindirish) operator
+sonlar = list(range(1, 11))
+# for son in sonlar:
+#     if son == 5:
+#         break
+
+#     print(f"{son} ning kvadrati {son**2} ga teng")
+
+# print("Kiritilgan sonning kvadratini qaytaruvchi dastur.")
+# savol = "Istalgan son kiriting "
+# savol += "(dasturni to'xtatish uchun 'exit' deb yozing): "
+
+# while True: # abadiy tsikl
+#     qiymat = input(savol)
+#     if qiymat == 'exit':
+#         break # tsiklni to'xtatish
+#     else:
+#         print(float(qiymat)**2)
+
+# continue(davom etish) operator
+# for son in sonlar:
+#     if son == 7:
+#         continue
+
+#     print(f"{son} ning kvadrati {son**2} ga teng")
+
+# son = 0
+# while son < 10:
+#     son += 1
+#     if son % 2 != 0:
+#         continue
+#     else:
+#         print(son)
+
+# infinite loop
+# son = 0
+# while son<10:
+#     son += 1
+#     if son%2!=0:
+#         continue
+#     else:
+#         print(son)
+
+# son = 1
+# while son < 50: 
+#     son += 1
+#     if son%2!=0:
+#         continue
+#     else:
+#         print(son)
+
+book = input("Yaxshi ko'rgan kitob nomini kiriting: ")
+
+while book != 'stop':
+    if book == 'stop':
+        break
+    else: 
+        print(book)
